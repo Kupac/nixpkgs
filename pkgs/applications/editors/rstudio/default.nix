1ec12879227cadc2de9deea69c1c0773dc1adacc
Bruno Rodrigues <bruno@brodrigues.co>
2023-07-23 23:33:25 +0200

    typo in path

--- pkgs/applications/editors/rstudio/default.nix
+++ pkgs/applications/editors/rstudio/default.nix
@@ -186,7 +186,7 @@ in
       ( cd dependencies && ${R}/bin/R CMD build -d --no-build-vignettes rsconnect )
 
       mkdir -p /build/source/src/gwt/lib/quarto
-      mkdir -p /build/source/src/gwt/lib/quarto/apss/panmirror/
+      mkdir -p /build/source/src/gwt/lib/quarto/apps/panmirror/
       cp -r ${panmirrorSrc} /build/source/src/gwt/lib/quarto/
       cp -r "${panmirrorModules}" /build/source/src/gwt/lib/quarto/apps/panmirror/node_modules
     '';
